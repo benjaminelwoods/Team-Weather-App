cdd02756fef9a8d081ffc22b18ebfa5063525304
Ben Elwood <ben.elwoods@gmail.com>
2020-07-31 10:17:09 +1200

    user input and animations

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,14 @@
 (function(){
 
+  const inputContainer = document.getElementById('userInput')
+  // const cityInput = document.getElementById('cityInput')
+  // const countryInput = document.getElementById('countryInput')
+  const inputBtn = document.getElementById('inputBtn')
+  const appContainer = document.getElementById('app')
+
+  let lat
+  let long
+
   let app = new Vue({
     el: '#app',
     data: {
@@ -28,21 +37,76 @@
   });
 
    function makeAjaxRequest () {
+
+     let dataObj = {
+       lat: '',
+       long: ''
+     }
+
+     inputBtn.onclick = function () {
+       const city = document.getElementById('cityInput').value
+       const country = document.getElementById('countryInput').value
+       inputContainer.classList.remove('showMe')
+       inputContainer.classList.add('hideMe')
+       appContainer.classList.remove('hideMe')
+       appContainer.classList.add('showMe')
+
+       setTimeout(function() {
+           inputContainer.css('display', 'none !important');
+       }, 1500);
+
+       axios({
+         method: 'get',
+         url: 'http://open.mapquestapi.com/geocoding/v1/address?key=AeBQ5pBycCJk58NtTLBHf9J3n4vfcUZ1&location=' + city + ',' + country
+       }).then(function (response) {
+         // console.log()
+         dataObj.lat = response.data.results[0].locations[0].latLng.lat
+         dataObj.long = response.data.results[0].locations[0].latLng.lng
+         updateData()
+         console.log(dataObj)
+       });
+     }
       // GET request for remote image in node.js
       function getLocation () {
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition)
+          navigator.geolocation.getCurrentPosition(updateData)
+          console.log(navigator.geolocation)
         } else {
           console.log('not working')
         }
       }
 
+      function updateData (position) {
+        if (position != undefined) {
+          dataObj.lat = position.coords.latitude
+          dataObj.long = position.coords.longitude
+          inputContainer.classList.remove('showMe')
+          inputContainer.classList.add('hideMe')
+          appContainer.classList.remove('hideMe')
+          appContainer.classList.add('showMe')
+          showPosition()
+        } else {
+          showPosition()
+        }
+      }
+
       getLocation()
 
-      function showPosition (position) {
+      function showPosition () {
+        let finalLat
+        let finalLong
+
+        if ((lat != null) && (long != null)) {
+          finalLat = lat
+          finalLat = long
+        } else {
+          finalLat = dataObj.lat
+          finalLong = dataObj.long
+        }
+
         axios({
           method: 'get',
-          url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&exclude=hourly,minute&appid=66ce6f7e945db003aaa343f0bc010dc8'
+          url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + finalLat + '&lon=' + finalLong + '&units=metric&exclude=hourly,minute&appid=66ce6f7e945db003aaa343f0bc010dc8'
         }).then(function (response) {
           app.weather = response
           function getIcons () {
@@ -59,7 +123,7 @@
         });
         axios({
           method: 'get',
-          url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + position.coords.longitude + ',' + position.coords.latitude + '.json?access_token=pk.eyJ1IjoiYmVuZWx3b29kcyIsImEiOiJja2I1b3J3NWwxNzJzMnRzN2J4cXV0cDZyIn0.NfLf39V4gp_PPtreLXVffQ'
+          url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + dataObj.long + ',' + dataObj.lat + '.json?access_token=pk.eyJ1IjoiYmVuZWx3b29kcyIsImEiOiJja2I1b3J3NWwxNzJzMnRzN2J4cXV0cDZyIn0.NfLf39V4gp_PPtreLXVffQ'
         }).then(function (response) {
           app.location = response
         });
